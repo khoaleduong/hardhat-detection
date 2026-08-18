da434d7b09eb200eac36c04dc91b0f8a844f0037
Le Duong Khoa <leduongkhoa1234@gmail.com>
2026-08-19 00:35:01 +0700

    Updated evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,36 +1,31 @@
 import torch
+from config import DATA_DIR, BATCH_SIZE, NUM_WORKERS, PIN_MEMORY, CLASS_OFFSET, NUM_CLASSES, CLASS_NAMES, TRAINABLE_BACKBONE_LAYERS, BEST_MODEL_PATH, DEVICE
 from src.faster_rcnn_model import build_model
 from src.dataset.dataloader import build_dataloader
 from src.checkpoint import load_checkpoint
 from src.engine import evaluate
-from config import DATA_DIR, BATCH_SIZE, NUM_WORKERS, PIN_MEMORY, CLASS_OFFSET, NUM_CLASSES, CLASS_NAMES, PRETRAINED, TRAINABLE_BACKBONE_LAYERS, BEST_MODEL_PATH, DEVICE
-
-
-CLASS_NAMES = ["background", "helmet", "head", "person"]
 
 
 def main():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("=" * 60)
+    print("Evaluating best checkpoint")
+    print(f"Device: {DEVICE}")
+    print(f"Checkpoint: {BEST_MODEL_PATH}")
+    print("=" * 60)
 
-    # Data
-    _, valid_loader = build_dataloader(
-        data_dir=DATA_DIR,
-        batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-        class_offset=CLASS_OFFSET,
-        num_classes=NUM_CLASSES,
-    )
+    # 1. Validation DataLoader
+    _, valid_loader = build_dataloader(data_dir=DATA_DIR, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, pin_memory=PIN_MEMORY, class_offset=CLASS_OFFSET, num_classes=NUM_CLASSES)
+    print(f"Validation samples: {len(valid_loader.dataset)}") # type: ignore
 
-    # Model
-    model = build_model(num_classes=NUM_CLASSES, pretrained=PRETRAINED, trainable_backbone_layers=TRAINABLE_BACKBONE_LAYERS)
-    model.to(device)
+    # 2. Build model
+    model = build_model(num_classes=NUM_CLASSES, pretrained=False, trainable_backbone_layers=TRAINABLE_BACKBONE_LAYERS)
+    model.to(DEVICE)
 
-    # Load best checkpoint
-    load_checkpoint(checkpoint_path=BEST_MODEL_PATH, model=model, device=device)  # type: ignore
+    # 3. Load best checkpoint
+    load_checkpoint(checkpoint_path=BEST_MODEL_PATH, model=model, device=DEVICE) # type: ignore
 
-    # Evaluation
-    evaluate(model=model, loader=valid_loader, device=device, class_names=CLASS_NAMES)
+    # 4. Evaluate
+    evaluate(model=model, loader=valid_loader, device=DEVICE, class_names=CLASS_NAMES)
 
 
 if __name__ == "__main__":
